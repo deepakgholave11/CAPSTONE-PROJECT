a582c35ec6f55dc8c78b8fefeb1aa0c33c314d0b
Deepak Gholave <deepakgholave11@gmail.com>
2026-02-21 17:21:02 +0530

    Modify EBS CSI driver for new cluster setup
    Updated EBS CSI driver configuration for new cluster and added IAM role and policy for EBS CSI.

--- terraform/main.tf
+++ terraform/main.tf
@@ -184,13 +184,37 @@ resource "aws_eks_node_group" "main" {
 # ---------------- EBS CSI ADDON ----------------
 
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name   = "aws-ebs-csi-driver"
-
+  cluster_name             = aws_eks_cluster.deepak.name
+  addon_name               = "aws-ebs-csi-driver"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
-  depends_on = [
-    aws_eks_node_group.main
-  ]
+  service_account_role_arn = aws_iam_role.ebs_csi_role.arn
+}
+
+resource "aws_iam_role" "ebs_csi_role" {
+  name = "deepak-eks-ebs-csi-role"
+
+  assume_role_policy = data.aws_iam_policy_document.ebs_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ebs_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_eks_cluster.deepak.identity[0].oidc[0].issuer, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  role       = aws_iam_role.ebs_csi_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
