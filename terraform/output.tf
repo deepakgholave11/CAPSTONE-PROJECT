44320f388a52b6b801cc3fba845120cafaea9897
Deepak Gholave <deepakgholave11@gmail.com>
2026-02-21 15:06:56 +0530

    Update EKS outputs with new resource references

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,15 +1,29 @@
+output "cluster_name" {
+  description = "EKS Cluster Name"
+  value       = aws_eks_cluster.main.name
+}
+
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  description = "EKS Cluster ID"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_endpoint" {
+  description = "EKS Cluster API Endpoint"
+  value       = aws_eks_cluster.main.endpoint
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  description = "EKS Node Group ID"
+  value       = aws_eks_node_group.main.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  description = "VPC ID"
+  value       = aws_vpc.main.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  description = "Subnet IDs"
+  value       = aws_subnet.main[*].id
 }
